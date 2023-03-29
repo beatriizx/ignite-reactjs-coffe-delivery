f626e2274343e78391ba011d1843e361c565219e
Beatriz <beatriz.lira@skeelo.app>
2023-03-29 20:58:21 -0300

    feat: create pages and  update routers

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,10 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import { Header } from '../components/Header';
 import { DefaultLayout } from '../layouts/DefaultLayout';
+import { Cart } from '../pages/Cart';
+import { Checkout } from '../pages/Checkout';
+import { Home } from '../pages/Home';
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}></Route>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Route>
     </Routes>
   );
 };

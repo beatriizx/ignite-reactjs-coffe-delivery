c27a385f819ec89492750d21d6c9edd0927272bd
Beatriz <beatriz.lira@skeelo.app>
2023-04-04 20:41:55 -0300

    feat: created json server to retrieve the products

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,22 +1,32 @@
 import styles from './styles.module.scss';
 import cartIcon from '../../assets/cart-icon-3.svg';
-import coffeImage from '../../assets/coffe.svg';
-export const ProductCard = () => {
-  const productTag = (text: string) => <span className={styles.productTag}>{text}</span>;
+
+interface ProductCardProps {
+  product: {
+    name: string;
+    description: string;
+    price: number;
+    tags: string[];
+    image: string;
+  };
+}
+export const ProductCard = ({ product }: ProductCardProps) => {
+  const productTag = (text: string) => <span>{text}</span>;
+  const { name, description, price, tags } = product;
   return (
     <div className={styles.cardContainer}>
       <div className={styles.cardContent}>
-        <img src={coffeImage} alt="" className={styles.productImage} />
+        <img src={product.image} alt="" className={styles.productImage} />
 
         <div className={styles.productInfo}>
-          {productTag('Tradicional')}
-          <h2>Expresso Tradicional</h2>
-          <p>O tradicional café feito com água quente e grãos moídos</p>
+          <div className={styles.productTag}>{tags.map((tag) => productTag(tag))}</div>
+          <h3>{name}</h3>
+          <p>{description}</p>
         </div>
 
         <div className={styles.priceContainer}>
           <span>
-            R$ <b>9,90</b>
+            R$ <b>{price}</b>
           </span>
           <div className={styles.productControllers}>
             <button>-</button>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,34 @@
 import styles from './styles.module.scss';
-
+import axios from 'axios';
 import { IntroSection } from '../Home/Intro';
 import { ProductCard } from '../../components/ProductCard';
+import { useEffect, useState } from 'react';
 
 export const Home = () => {
+  const [products, setProducts] = useState([]);
+
+  const retrieveProducts = async () => {
+    const { data } = await axios.get('http://localhost:3000/products');
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    retrieveProducts();
+  }, []);
+
   return (
     <div className={styles.homeContainer}>
       <div className={styles.homeContent}>
         <IntroSection />
-        <ProductCard />
+
+        <h2>Nossos cafés</h2>
+        <ul className={styles.productList}>
+          {products.map((product) => (
+            <li>
+              <ProductCard product={product} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );

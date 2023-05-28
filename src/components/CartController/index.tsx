0a3bef48b36c499e18b19921a6c0519add8fb39a
Beatriz <beatriz.lira@skeelo.app>
2023-05-28 15:18:02 -0300

    success message when adding product to cart

--- src/components/CartController/index.tsx
+++ src/components/CartController/index.tsx
@@ -1,65 +1,72 @@
+/** @format */
 import { useContext, useEffect, useState } from 'react';
 import { CartContext } from '../../contexts/CartContext';
 import styles from './styles.module.scss';
 import { Product } from '../../contexts/CartContext';
 import cartIcon from '../../assets/cart-icon-3.svg';
 import trashIcon from '../../assets/trash-icon.svg';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface CartControllerProps {
-  product: Product;
-  pageType: string;
+	product: Product;
+	pageType: string;
 }
 
 export const CartController = ({ product, pageType }: CartControllerProps) => {
-  const [productAmount, setProductAmount] = useState<number>(product.amount || 1);
-  const { addNewProductToCart, removeProductToCart, handleProductCartAmountChange } =
-    useContext(CartContext);
-  const buttonIcon = pageType === 'cart' ? trashIcon : cartIcon;
+	const [productAmount, setProductAmount] = useState<number>(product.amount || 1);
+	const { addNewProductToCart, removeProductToCart, handleProductCartAmountChange } =
+		useContext(CartContext);
+	const buttonIcon = pageType === 'cart' ? trashIcon : cartIcon;
 
-  const productProps = {
-    id: product.id,
-    name: product.name,
-    price: product.price,
-    imageSrc: product.imageSrc,
-    amount: productAmount,
-  };
+	const productProps = {
+		id: product.id,
+		name: product.name,
+		price: product.price,
+		imageSrc: product.imageSrc,
+		amount: productAmount,
+	};
 
-  const handleCartChange = pageType === 'cart' ? removeProductToCart : addNewProductToCart;
+	const handleCartChange = pageType === 'cart' ? removeProductToCart : addNewProductToCart;
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.productControllers}>
-        <button
-          onClick={() => {
-            setProductAmount((amount) => amount - 1);
-            if (pageType === 'cart') handleProductCartAmountChange(productProps, false);
-          }}
-          disabled={productAmount === 1}
-          type="button"
-        >
-          -
-        </button>
-        <span>{productAmount}</span>
-        <button
-          onClick={() => {
-            setProductAmount((amount) => amount + 1);
-            if (pageType === 'cart') handleProductCartAmountChange(productProps);
-          }}
-          type="button"
-        >
-          +
-        </button>
-      </div>
-      <button
-        className={styles.buttonAction}
-        onClick={() => {
-          handleCartChange(productProps, true);
-        }}
-        type="button"
-      >
-        <img src={buttonIcon} alt="" />
-        {pageType === 'cart' && <span>REMOVER</span>}
-      </button>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+      <ToastContainer />
+			<div className={styles.productControllers}>
+				<button
+					onClick={() => {
+						setProductAmount((amount) => amount - 1);
+						if (pageType === 'cart') handleProductCartAmountChange(productProps, false);
+					}}
+					disabled={productAmount === 1}
+					type='button'
+				>
+					-
+				</button>
+				<span>{productAmount}</span>
+				<button
+					onClick={() => {
+						setProductAmount((amount) => amount + 1);
+						if (pageType === 'cart') handleProductCartAmountChange(productProps);
+					}}
+					type='button'
+				>
+					+
+				</button>
+			</div>
+			<button
+				className={styles.buttonAction}
+				onClick={() => {
+					handleCartChange(productProps, true);
+				}}
+				type='button'
+			>
+				<img
+					src={buttonIcon}
+					alt=''
+				/>
+				{pageType === 'cart' && <span>REMOVER</span>}
+			</button>
+		</div>
+	);
 };
--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,95 +1,98 @@
-import { createContext, ReactNode, useEffect, useState } from 'react';
+/** @format */
 
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 interface CartContextProviderProps {
-  children: ReactNode;
+	children: ReactNode;
 }
 export type Product = {
-  id: number;
-  name: string;
-  price: number;
-  imageSrc: string;
-  amount: number;
+	id: number;
+	name: string;
+	price: number;
+	imageSrc: string;
+	amount: number;
 };
 
 export const CartContext = createContext({} as any);
 export const CartContextProvider = ({ children }: CartContextProviderProps) => {
-  const [products, setProducts] = useState<Product[]>([]);
+	const [products, setProducts] = useState<Product[]>([]);
 
-  const addNewProductToCart = (currentProduct: Product) => {
-    const index = products.findIndex((product) => currentProduct.id === product.id);
+	const addNewProductToCart = (currentProduct: Product) => {
+		const index = products.findIndex((product) => currentProduct.id === product.id);
 
-    if (index !== -1) {
-      let newProcutList = [...products];
-      const oldProductAmount = newProcutList[index].amount;
-      newProcutList[index].amount = currentProduct.amount + oldProductAmount;
-      setProducts(newProcutList);
-      return;
-    }
+		if (index !== -1) {
+			let newProcutList = [...products];
+			const oldProductAmount = newProcutList[index].amount;
+			newProcutList[index].amount = currentProduct.amount + oldProductAmount;
+			setProducts(newProcutList);
+			return;
+		}
 
-    setProducts((oldProduct) => [...oldProduct, currentProduct]);
-  };
+		setProducts((oldProduct) => [...oldProduct, currentProduct]);
+		toast.success('Produto adicionado ao seu carrinho!');
+	};
 
-  const removeProductToCart = (currentProduct: Product) => {
-    const newArr = products.filter((product) => currentProduct.id !== product.id);
-    setProducts(newArr);
-  };
+	const removeProductToCart = (currentProduct: Product) => {
+		const newArr = products.filter((product) => currentProduct.id !== product.id);
+		setProducts(newArr);
+	};
 
-  const cleanCart = () => {
-    setProducts([]);
-  };
+	const cleanCart = () => {
+		setProducts([]);
+	};
 
-  const updateLocalStorage = () => {
-    const value = JSON.stringify(products);
-    localStorage.setItem('coffe-delivery-cart', value);
-  };
+	const updateLocalStorage = () => {
+		const value = JSON.stringify(products);
+		localStorage.setItem('coffe-delivery-cart', value);
+	};
 
-  const handleProductCartAmountChange = (currentProduct: Product, increase: boolean = true) => {
-    const product = products.findIndex((product) => currentProduct.id === product.id);
-    let newProcutList = [...products];
-    newProcutList[product].amount = increase
-      ? currentProduct.amount + 1
-      : currentProduct.amount - 1;
-    setProducts(newProcutList);
-  };
+	const handleProductCartAmountChange = (currentProduct: Product, increase: boolean = true) => {
+		const product = products.findIndex((product) => currentProduct.id === product.id);
+		let newProcutList = [...products];
+		newProcutList[product].amount = increase
+			? currentProduct.amount + 1
+			: currentProduct.amount - 1;
+		setProducts(newProcutList);
+	};
 
-  const totalProductsInCart = products.reduce((total, item) => (total += item.amount), 0);
+	const totalProductsInCart = products.reduce((total, item) => (total += item.amount), 0);
 
-  const totalProductsPrice = products
-    .reduce((total, item) => (total += item.price * item.amount), 0)
-    .toFixed(2);
+	const totalProductsPrice = products
+		.reduce((total, item) => (total += item.price * item.amount), 0)
+		.toFixed(2);
 
-  const shippingPrice = 3.5;
+	const shippingPrice = 3.5;
 
-  const totalPrice = (Number(totalProductsPrice) + shippingPrice).toFixed(2);
+	const totalPrice = (Number(totalProductsPrice) + shippingPrice).toFixed(2);
 
-  useEffect(() => {
-    const retrieveProducts = localStorage.getItem('coffe-delivery-cart');
+	useEffect(() => {
+		const retrieveProducts = localStorage.getItem('coffe-delivery-cart');
 
-    if (retrieveProducts && JSON.parse(retrieveProducts).length > 0) {
-      setProducts(JSON.parse(retrieveProducts));
-      return;
-    }
-  }, []);
+		if (retrieveProducts && JSON.parse(retrieveProducts).length > 0) {
+			setProducts(JSON.parse(retrieveProducts));
+			return;
+		}
+	}, []);
 
-  useEffect(() => {
-    updateLocalStorage();
-  }, [products]);
+	useEffect(() => {
+		updateLocalStorage();
+	}, [products]);
 
-  return (
-    <CartContext.Provider
-      value={{
-        totalProductsInCart,
-        addNewProductToCart,
-        handleProductCartAmountChange,
-        removeProductToCart,
-        cleanCart,
-        totalProductsPrice,
-        totalPrice,
-        shippingPrice,
-        products,
-      }}
-    >
-      {children}
-    </CartContext.Provider>
-  );
+	return (
+		<CartContext.Provider
+			value={{
+				totalProductsInCart,
+				addNewProductToCart,
+				handleProductCartAmountChange,
+				removeProductToCart,
+				cleanCart,
+				totalProductsPrice,
+				totalPrice,
+				shippingPrice,
+				products,
+			}}
+		>
+			{children}
+		</CartContext.Provider>
+	);
 };
